6cf13fa015b0691aa744558dd0a9f70a8b0c95c6
John Dzak <john.dzak@gmail.com>
2013-01-21 21:59:46 -0600

    cleanup

--- mini_listener.rb
+++ mini_listener.rb
@@ -5,13 +5,3 @@ req.send do |response|
 	puts "mini is active!!"
 	puts response.inspect
 end
-
-
-# require 'open3'
-# Open3.popen3('dns-sd -L mini _plexmediasvr') {|stdin, stdout, stderr, wait_thr|
-# puts stdin
-# while(a = stdout.gets)
-# 	puts a
-# end
-# puts stderr
-# 	}
