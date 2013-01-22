926b5ad861de5eb612c4ac25cdc7fe6a0b3890ab
John Dzak <john.dzak@gmail.com>
2013-01-21 21:57:35 -0600

    update listener file

--- mini_listener.rb
+++ mini_listener.rb
@@ -1,7 +1,8 @@
-require './dns_sd'
+require File.dirname(__FILE__) + '/dns_sd'
 
 req = DnsSd::LookupRequest.new('mini', '_plexmediasvr')
 req.send do |response|
+	puts "mini is active!!"
 	puts response.inspect
 end
 
